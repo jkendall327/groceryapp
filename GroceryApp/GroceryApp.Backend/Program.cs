28e7c49bdef7ce35521b64e014290e4ad9af28be
jackkendall <jkendall3096@gmail.com>
2025-02-23 13:47:01 +0000

    Clean up Program.cs

--- GroceryApp/GroceryApp.Backend/Program.cs
+++ GroceryApp/GroceryApp.Backend/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Storage.Blobs;
+using GroceryApp.Backend;
 using Microsoft.Azure.Cosmos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,22 +37,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast",
-        () =>
-        {
-            var forecast = Enumerable.Range(1, 5)
-                .Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]))
-                .ToArray();
-            return forecast;
-        })
-    .WithName("GetWeatherForecast");
 
 app.Run();
